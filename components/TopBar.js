981ec8a8ffcfdfcd3bfe34aeefc3c5ff571e4e5a
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2023-02-03 18:29:22 -0300

    Desenvolvendo o header

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,7 +1,11 @@
+import styles from '@/styles/TopBar.module.css'
+import Link from 'next/link';
 
-
-export default function TopBar() {
+export default function TopBar({title, icon, isHome = false}) {
     return (
-        <header>TopBar</header>
+        <header className={styles.TopBar}>
+            <h1>{icon || <i className="fa-solid fa-calculator"></i>} <span className={styles.title}>{title || "Calculadoras que ajudam você!"}</span></h1>
+            {!isHome || <Link href={"/"}><i class="fa-solid fa-caret-left"></i> Voltar para home</Link>}
+        </header>
     );
 }
