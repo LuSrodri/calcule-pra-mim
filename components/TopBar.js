39ac346f9be75deacf191e8bf3fb0d67b977dd02
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2023-02-03 21:34:29 -0300

    Aprimorando a TopBar

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,11 +1,11 @@
 import styles from '@/styles/TopBar.module.css'
 import Link from 'next/link';
 
-export default function TopBar({title, icon, isHome = false}) {
+export default function TopBar({title, icon, isHome = true}) {
     return (
         <header className={styles.TopBar}>
-            <h1>{icon || <i className="fa-solid fa-calculator"></i>} <span className={styles.title}>{title || "Calculadoras que ajudam você!"}</span></h1>
-            {!isHome || <Link href={"/"}><i class="fa-solid fa-caret-left"></i> Voltar para home</Link>}
+            <h1>{icon || <i className="fa-solid fa-calculator"></i>} <span className={isHome || styles.show}>{title || "Calculadoras que ajudam você!"}</span></h1>
+            {isHome || <Link href={"/"}><i className="fa-solid fa-house"></i> <span className={styles.show}>Voltar para home</span></Link>}
         </header>
     );
 }
