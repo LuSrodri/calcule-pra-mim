aaf12ac4c34c2d8becdbbd36ff0a4e8fa5945b31
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2023-02-03 21:59:42 -0300

    Desenvolvendo o Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,10 @@
-
+import Link from "next/link";
+import styles from '@/styles/Footer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
 export default function Footer() {
     return (
-        <footer>Footer</footer>
+        <footer className={styles.Footer}><Link href={"https://lusrodri.me"} target="_blank">Desenvolvido por LuSRodri <FontAwesomeIcon icon={faUpRightFromSquare}/></Link></footer>
     );
 }
--- components/TopBar.js
+++ components/TopBar.js
@@ -1,11 +1,13 @@
 import styles from '@/styles/TopBar.module.css'
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalculator, faHouse } from '@fortawesome/free-solid-svg-icons'
 
 export default function TopBar({title, icon, isHome = true}) {
     return (
         <header className={styles.TopBar}>
-            <h1>{icon || <i className="fa-solid fa-calculator"></i>} <span className={isHome || styles.show}>{title || "Calculadoras que ajudam você!"}</span></h1>
-            {isHome || <Link href={"/"}><i className="fa-solid fa-house"></i> <span className={styles.show}>Voltar para home</span></Link>}
+            <h1>{icon || <FontAwesomeIcon icon={faCalculator} />} <span className={isHome ? "" : styles.show}>{title || "Calculadoras que ajudam você!"}</span></h1>
+            {isHome || <Link href={"/"}><FontAwesomeIcon icon={faHouse} /> <span className={styles.show}>Voltar para home</span></Link>}
         </header>
     );
 }
