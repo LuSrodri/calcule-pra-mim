290697d2efb793356277def4ef797f4e9b87a9cb
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2023-07-22 09:27:43 -0300

    adding NEXT_PUBLIC_LINK to append affiliate links

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,11 @@
 import styles from '@/styles/Footer.module.css';
+require('dotenv').config();
 
 export default function Footer() {
     return (
-        <footer className={styles.Footer}>Índice de massa corporal, peso ideial, juros compostos e diferenças entre duas datas - Calcule pra mim!</footer>
+        <footer className={styles.Footer}>
+            <p className={styles.plink} dangerouslySetInnerHTML={{__html: process.env.NEXT_PUBLIC_LINK}}></p>
+            <p>Índice de massa corporal, peso ideial, juros compostos e diferenças entre duas datas - Calcule pra mim!</p>
+        </footer>
     );
 }
