27e9ce3bca4afc1829b6eea6f5a3c067228be17c
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2024-02-05 20:54:09 -0300

    removing gtags

--- pages/_app.js
+++ pages/_app.js
@@ -7,20 +7,6 @@ config.autoAddCss = false
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ZEYP70KP8E" />
-      <Script id="gtag">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag() { dataLayer.push(arguments); }
-          gtag('js', new Date());
-
-          gtag('config', 'G-ZEYP70KP8E');
-        `}
-      </Script>
-      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4463881368194285"
-        crossorigin="anonymous"></Script>
-      <Component {...pageProps} />
-    </>
+    <Component {...pageProps} />
   )
 }
